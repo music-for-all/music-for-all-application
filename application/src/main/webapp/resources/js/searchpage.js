dd44ddf10e705beda8b8cfb356955d5fe4f5ee38
Pavel Podgorniy <podgpavel@gmail.com>
2016-06-13 19:46:47 +0300

    Added search page

---
+++ application/src/main/webapp/resources/js/searchpage.js
@@ -0,0 +1,27 @@
+function addRow(Artist, Title, Duration){
+    $('#results').append('<tr><td> </td><td>' + Artist + ' </td><td>' + Title + ' </td><td>' + Duration + ' </td></tr>');
+}
+
+function clearAll(){
+    $("#results").find("tr:not(:first)").remove();
+}
+
+function dummy(searchQuery, count){ //Only for demonstration, delete after merging with ajax
+    clearAll();
+    for(var i=0; i<parseInt(count);i++){
+        addRow(searchQuery, searchQuery, "2:22");
+    }
+}
+function search(){
+    var searchQuery = $("#word").val(); //Key-word for searching
+    var selectedCategory = []; //Array with categories
+    $("input:checkbox[name=category]:checked").each(function(){
+        selectedCategory.push($(this).val());
+    });
+    console.log("Query for searching: " + searchQuery);
+    console.log("Genres for searching: " + selectedCategory);
+    if(searchQuery!="") {
+        dummy(searchQuery,15);
+    }
+}
+
