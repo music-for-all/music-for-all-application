aab1c968b7b2f25e8dffb08ee39264e2408608c5
ENikolskiy <enikolskiy@luxoft.com>
2016-06-14 19:17:09 +0300

    Add logger.

--- application/src/main/java/com/musicforall/web/FileController.java
+++ application/src/main/java/com/musicforall/web/FileController.java
@@ -1,6 +1,8 @@
 package com.musicforall.web;
 
 import com.musicforall.files.manager.FileManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ import java.nio.file.Path;
 
 @Controller
 public class FileController {
+    private static final Logger LOG = LoggerFactory.getLogger(FileController.class);
 
     @Autowired
     private FileManager manager;
@@ -41,7 +44,7 @@ public class FileController {
         try {
             Files.copy(filePath, response.getOutputStream());
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Streaming failed!", e);
         }
     }
 
