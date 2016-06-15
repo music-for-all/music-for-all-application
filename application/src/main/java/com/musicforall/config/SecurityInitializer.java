4d1143c3fc9db2f1efe39137d7fcf4030404c74f
Julia <pustovayajulia@gmail.com>
2016-06-15 19:59:39 +0400

    Add Spring Secutity configuration classes

---
+++ application/src/main/java/com/musicforall/config/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.musicforall.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
+
--- application/src/main/java/com/musicforall/config/WebAppConfig.java
+++ application/src/main/java/com/musicforall/config/WebAppConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.musicforall.web")
+@Import(SecurityConfig.class)
 public class WebAppConfig {
 	@Bean
 	public VelocityViewResolver viewResolver() {
@@ -20,6 +21,7 @@ public class WebAppConfig {
 		resolver.setPrefix("");
 		resolver.setSuffix(".vm");
 		resolver.setViewClass(VelocityView.class);
+		resolver.setContentType("text/html; charset=utf-8");
 		return resolver;
 	}
 
