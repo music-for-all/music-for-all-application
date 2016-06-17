9ea940ebec8a7b84bf7776834b78c31de4efbe0e
Pavel Podgorniy <podgpavel@gmail.com>
2016-06-17 18:41:10 +0300

    Added standart login form to welcompage Added deleting playlists Fixed a lot of bugs

--- application/src/main/webapp/resources/js/mainpage.js
+++ application/src/main/webapp/resources/js/mainpage.js
@@ -1,4 +1,4 @@
-function addRow(Artist, Title, Duration){
+function addRow(Artist, Title, Duration) {
     $('#results').append('<tr><td><button type="button" class="btn btn-xs btn-success">' +
         '<span class="glyphicon glyphicon-play" aria-hidden="true"></span></button> ' +
         '<button type="button" class="btn btn-xs btn-danger" onclick="DeleteSongFunction(this)"> ' +
@@ -6,9 +6,9 @@ function addRow(Artist, Title, Duration){
         '<td>' + Artist + ' </td><td>' + Title + ' </td><td>' + Duration + ' </td></tr>');
 }
 
-function addRowArray(Artist, Title, Duration){
+function addRowArray(Artist, Title, Duration) {
 
-    for(var i=0; i<Title.length;i++) {
+    for (var i = 0; i < Title.length; i++) {
         $('#results').append('<tr><td><button type="button" class="btn btn-xs btn-success">' +
             '<span class="glyphicon glyphicon-play" aria-hidden="true"></span></button> ' +
             '<button type="button" class="btn btn-xs btn-danger" onclick="DeleteSongFunction(this)"> ' +
@@ -17,35 +17,40 @@ function addRowArray(Artist, Title, Duration){
     }
 }
 
-function clearAll(){
+function clearAll() {
     $("#results").find("tr:not(:first)").remove();
 }
 
 function DeleteSongFunction(o) {
-    var p=o.parentNode.parentNode;
+    var p = o.parentNode.parentNode;
     p.parentNode.removeChild(p);
     //Write: Send request for deleting song to server
 }
 
-function clearAllPlaylists(){
+function clearAllPlaylists() {
     $("#playlists").find("li:not(:first)").remove();
 }
 
-function addPlaylist(Name){
-    $('#playlists').append(' <li ><a href="#" data-value="' +Name + '">' + Name +'</a></li>');
+function addPlaylist(Name) {
+    if (Name == "") {
+        Name = "Untitled";
+    }
+    if (Name.length > 20) {
+        Name = Name.substr(0, 20); //Trimming long line
+    }
+    $('#playlists').append(' <li ><a href="#" data-value="' + Name + '">' + Name + '</a></li>');
 }
 
-function dummy(){ //Only for demonstration, delete after merging with ajax
-    var min=2;
-    var max=15;
+function dummy() { //Only for demonstration, delete after merging with ajax
+    var min = 2;
+    var max = 15;
     var rand = min + Math.random() * (max - min)
     rand = Math.round(rand);
     clearAll();
-    for(var i=0; i<rand;i++){
+    for (var i = 0; i < rand; i++) {
         addRow("test data", "test data", "2:22");
     }
 }
-
 
 
 
