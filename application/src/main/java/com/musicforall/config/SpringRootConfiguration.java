54aeb3788d6366d50aa281c07d9c36e4a3c809aa
GodBlessCode <nastuha@3g.ua>
2016-06-17 18:06:43 +0300

    update config file

--- application/src/main/java/com/musicforall/config/SpringRootConfiguration.java
+++ application/src/main/java/com/musicforall/config/SpringRootConfiguration.java
@@ -1,6 +1,10 @@
 package com.musicforall.config;
 
 import com.musicforall.files.FileApiSpringConfig;
+import com.musicforall.model.Songlist;
+import com.musicforall.services.SongServiceImpl;
+import com.musicforall.services.SonglistServiceImpl;
+import com.musicforall.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
@@ -10,9 +14,13 @@ import org.springframework.core.env.Environment;
  * Created by kgavrylchenko on 10.06.16.
  */
 @Configuration
-@ComponentScan({"com.musicforall.common"})
+@ComponentScan({"com.musicforall.common",
+                "com.musicforall.services"})
 @Import({HibernateConfiguration.class,
-        FileApiSpringConfig.class})
+        FileApiSpringConfig.class,
+        UserServiceImpl.class,
+        SonglistServiceImpl.class,
+        SongServiceImpl.class})
 @PropertySource("classpath:/application.properties")
 public class SpringRootConfiguration {
 
