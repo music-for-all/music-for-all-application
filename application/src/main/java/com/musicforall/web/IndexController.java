4441254a926a9a356d9b93ce338c87faf4a0f4c4
GodBlessCode <nastuha@3g.ua>
2016-06-18 11:03:16 +0300

    added some tests in controller

--- application/src/main/java/com/musicforall/web/IndexController.java
+++ application/src/main/java/com/musicforall/web/IndexController.java
@@ -1,7 +1,15 @@
 package com.musicforall.web;
 
+import com.musicforall.model.Song;
+import com.musicforall.model.Songlist;
+import com.musicforall.model.Tag;
+import com.musicforall.model.User;
+import com.musicforall.services.SongService;
+import com.musicforall.services.SonglistService;
+import com.musicforall.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +19,18 @@ import java.util.Date;
 @Controller
 public class IndexController {
 
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private SonglistService songlistService;
+
+	@Autowired
+	private SongService songService;
+
 	private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
 
+	private int counter = 0;
 	public IndexController() {
 		LOG.debug("Sample Debug Message");
 	}
@@ -20,6 +38,51 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index(Model model) {
 		model.addAttribute("date", new Date());
+		if (counter==0) this.addSong();
+		counter++;
 		return "index";
 	}
+
+	private void addDefaultUser(){
+		userService.save(new User("JohnSmith26", "MyLoveIsNotU32", "JohnSmith@gmail.com"));
+	}
+
+
+	private void addDefaultAllSonglist(){
+		addDefaultUser();
+		Integer userId = userService.getIdByName("JohnSmith26");
+		songlistService.save(userId, "All");
+		songlistService.save(userId, "Not_all");
+	}
+
+
+	private void addSong(){
+		addDefaultAllSonglist();
+		Integer songlistId = songlistService.getSonglistId("All");
+		songService.save("somePath", songlistId);
+		songService.save("sonePath2", songlistId);
+		Integer userId = userService.getIdByName("JohnSmith26");
+		User u = userService.get(userId);
+		Integer song_id = songService.getSongId("somePath");
+
+
+
+		for (Songlist songlist:
+				userService.getAllUserSonglist(u)) {
+			LOG.info("Songl" + songlist.getName());
+		}
+
+		for (Song song: songlistService.getAllSongsInSonglist(songlistId)
+			 ) {
+			LOG.info("Song " + song.getName());
+		}
+
+	}
+
+	private void getSongListSong()
+	{
+
+
+	}
+
 }
