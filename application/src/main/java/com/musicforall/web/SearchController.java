06ae0ca128f7f5ddd60a99c711b1194b40b4d3eb
andrey <gorbandrey@i.ua>
2016-06-22 20:48:58 +0300

    Added stub for search

--- application/src/main/java/com/musicforall/web/SearchController.java
+++ application/src/main/java/com/musicforall/web/SearchController.java
@@ -1,10 +1,18 @@
 package com.musicforall.web;
 
+import com.musicforall.model.Song;
+import com.musicforall.model.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 public class SearchController {
@@ -19,5 +27,22 @@ public class SearchController {
 	public String welcome(Model model) {
 		LOG.debug("Requested /search");
 		return "search";
+	}
+
+	@RequestMapping(value = "/searchQuery", method = RequestMethod.GET)
+	@ResponseBody
+	public Set<Song> dummyFind(@RequestParam("search") String search) {
+		LOG.debug("Requested /searchQuery = " +search);
+		Set<Song> array = new HashSet<>();
+		Set<Tag> tag = new HashSet<>();
+		String location = "/home/andrey/MusicForAll";
+		Tag t = new Tag();
+		t.setName("music");
+		tag.add(t);
+		array.add(new Song(tag, search, location));
+		array.add(new Song(tag, "Nirvana", location));
+		array.add(new Song(tag, "Disturbed", location));
+		array.add(new Song(tag, "Rob Zombie", location));
+		return array;
 	}
 }
