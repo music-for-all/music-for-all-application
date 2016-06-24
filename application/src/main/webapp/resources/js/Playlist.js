076847fdd5b4a8371c468642a2e5a6ef2e5b8c69
kgavrylchenko <kgavrylchenko@luxoft.com>
2016-06-24 17:37:41 +0300

    refactored playlists javascript api to use self variable and built in promises

--- application/src/main/webapp/resources/js/Playlist.js
+++ application/src/main/webapp/resources/js/Playlist.js
@@ -2,61 +2,65 @@
  * @author ENikolskiy on 6/24/2016.
  */
 function Playlist() {
+    
+    var self = this;
+    
+    self.delete = function (id, header, token) {
+        var dfr = $.Deferred();
+        $.ajax({
+            type: "DELETE",
+            url: "/playlist",
+            data: {"id": id},
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(header, token);
+            },
+            success: function (response) {
+                dfr.resolve(response);
+            },
+            error: function () {
+                dfr.reject();
+            }
+        });
+        return dfr.promise();
+    };
+    
+    self.create = function (name, header, token) {
+        var dfr = $.Deferred();
+        return $.when($.post("/playlist/create", {"name": name}));
+
+        /*return $.ajax({
+            type: "POST",
+            url: ,
+            data: ,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(header, token);
+            },/!*
+            success: function (response) {
+                dfr.resolve(response);
+            },*!/
+            error: function () {
+                dfr.reject();
+            }
+        });*/
+        // return dfr.promise();
+    };
+    
+    self.songs = function (id, header, token) {
+        var dfr = $.Deferred();
+        $.ajax({
+            type: "GET",
+            url: "/playlist",
+            data: {"id": id},
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(header, token);
+            },
+            success: function (response) {
+                dfr.resolve(response);
+            },
+            error: function () {
+                dfr.reject();
+            }
+        });
+        return dfr.promise();
+    };
 }
-
-Playlist.prototype.delete = function (id, header, token) {
-    var dfr = $.Deferred();
-    $.ajax({
-        type: "DELETE",
-        url: "/playlist",
-        data: {"id": id},
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader(header, token);
-        },
-        success: function (response) {
-            dfr.resolve(response);
-        },
-        error: function () {
-            dfr.reject();
-        }
-    });
-    return dfr.promise();
-};
-
-Playlist.prototype.create = function (name, header, token) {
-    var dfr = $.Deferred();
-    $.ajax({
-        type: "POST",
-        url: "/playlist",
-        data: {"name": name},
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader(header, token);
-        },
-        success: function (response) {
-            dfr.resolve(response);
-        },
-        error: function () {
-            dfr.reject();
-        }
-    });
-    return dfr.promise();
-};
-
-Playlist.prototype.songs = function (id, header, token) {
-    var dfr = $.Deferred();
-    $.ajax({
-        type: "GET",
-        url: "/playlist",
-        data: {"id": id},
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader(header, token);
-        },
-        success: function (response) {
-            dfr.resolve(response);
-        },
-        error: function () {
-            dfr.reject();
-        }
-    });
-    return dfr.promise();
-};
