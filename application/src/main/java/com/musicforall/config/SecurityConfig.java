067d66fb462ec7cc47181df25f618159b229b0b9
Dimitri <excaliware@gmail.com>
2016-07-05 15:39:34 +0300

    Add AuthenticationProvider
    Add success and failure handlers.
    Set password encoder.
    Add the Remember-Me authentication.

--- application/src/main/java/com/musicforall/config/SecurityConfig.java
+++ application/src/main/java/com/musicforall/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.musicforall.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +26,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        final int REMEMBER_ME_SECONDS = 86400;  // 24h
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/", "/index", "/welcome*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/welcome").permitAll()
+                .successHandler(successHandler())
+                .failureHandler(failureHandler())
+                .and()
+                .rememberMe().tokenValiditySeconds(REMEMBER_ME_SECONDS)
+                .rememberMeParameter("_spring_security_remember_me")
                 .and()
                 .logout().permitAll();
     }
@@ -31,13 +46,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web
-                .ignoring()
-                .antMatchers("/resources/**");
+                .ignoring().antMatchers("/resources/**");
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+
+        SavedRequestAwareAuthenticationSuccessHandler authSuccessHandler =
+                new SavedRequestAwareAuthenticationSuccessHandler();
+        authSuccessHandler.setDefaultTargetUrl("/");
+        return authSuccessHandler;
+    }
+
+    @Bean
+    public AuthenticationFailureHandler failureHandler() {
+
+        SimpleUrlAuthenticationFailureHandler authFailureHandler = new SimpleUrlAuthenticationFailureHandler();
+        authFailureHandler.setDefaultFailureUrl("/welcome?error");
+        return authFailureHandler;
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.userDetailsService(userDetailsService);
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new StandardPasswordEncoder();
     }
 }
