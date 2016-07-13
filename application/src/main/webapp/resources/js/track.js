d77beca102b15863cbaf23d26d4daff8fbd32882
Pavel Podgorniy <podgpavel@gmail.com>
2016-07-13 17:16:59 +0300

    Replaced ajax part to JS file

--- application/src/main/webapp/resources/js/track.js
+++ application/src/main/webapp/resources/js/track.js
@@ -5,12 +5,36 @@ function Track() {
 
     var self = this;
 
-    self.createJson = function (tags, artist, name) {
+    self.createJson = function (tags, artist, name, file) {
         var obj = new Object();
         obj.tags = tags.split(",");
         obj.artist = artist;
         obj.name = name;
         obj.location = "unknown";
-        return JSON.stringify(obj);
+
+        var formData = new FormData();
+        formData.append('track', new Blob([JSON.stringify(obj)], {
+            type: "application/json"
+        }));
+        formData.append("file", file);
+        $.ajax({
+            url: "/files",
+            type: 'POST',
+            data: formData,
+            cache: false,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                showMessage(data, "success");
+                clearInputs();
+            },
+            error: function (xhr, status, error) {
+                if (xhr.responseText.length < max_length_error) {
+                    showMessage(xhr.responseText, "danger");
+                } else {
+                    showMessage(error, "danger");
+                }
+            }
+        });
     }
 }
