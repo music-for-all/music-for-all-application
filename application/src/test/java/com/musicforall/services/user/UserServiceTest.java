1b00e008fe73791a58f20e06a69bb5408e5c969a
kgavrylchenko <kgavrylchenko@luxoft.com>
2016-07-15 12:38:49 +0300

    codacy issues

--- application/src/test/java/com/musicforall/services/user/UserServiceTest.java
+++ application/src/test/java/com/musicforall/services/user/UserServiceTest.java
@@ -1,9 +1,7 @@
 package com.musicforall.services.user;
 
-import com.musicforall.common.dao.Dao;
 import com.musicforall.model.User;
 import com.musicforall.util.ServicesTestConfig;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,9 +43,6 @@ public class UserServiceTest {
 
     @Autowired
     private UserBootstrap userBootstrap;
-
-    @Autowired
-    Dao dao;
 
     @Test
     public void testSaveUser() {
