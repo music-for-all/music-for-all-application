681859c38549724ef3c4e5948bab1b30a313e0a1
Dimitri <excaliware@gmail.com>
2016-07-18 12:20:29 +0300

    Move the 'files' bean definition to FileApiSpringConfig

--- application/src/main/java/com/musicforall/config/SpringRootConfiguration.java
+++ application/src/main/java/com/musicforall/config/SpringRootConfiguration.java
@@ -1,10 +1,10 @@
 package com.musicforall.config;
 
 import com.musicforall.files.FileApiSpringConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by kgavrylchenko on 10.06.16.
@@ -18,13 +18,4 @@ import org.springframework.core.env.Environment;
         SecurityConfig.class})
 @PropertySource(value = "file:${user.home}/application.properties")
 public class SpringRootConfiguration {
-
-    @Autowired
-    private Environment env;
-
-    @Bean
-    @Qualifier("files")
-    public String filesDirectory() {
-        return env.getRequiredProperty("files.directory");
-    }
 }
