d290ff16b65db717b762c48166d15cf3d57f00dd
Dimitri <excaliware@gmail.com>
2016-07-27 16:02:15 +0300

    Move security-related classes to their own package

--- application/src/main/java/com/musicforall/config/security/CustomAuthenticationFailureHandler.java
+++ application/src/main/java/com/musicforall/config/security/CustomAuthenticationFailureHandler.java
@@ -1,4 +1,4 @@
-package com.musicforall.config;
+package com.musicforall.config.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
