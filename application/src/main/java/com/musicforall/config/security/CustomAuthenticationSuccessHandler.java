78841b9cf06eaae58be2d1d6adbfc10a0d0f196e
Dimitri <excaliware@gmail.com>
2016-07-27 20:23:52 +0300

    Refactor the code

--- application/src/main/java/com/musicforall/config/security/CustomAuthenticationSuccessHandler.java
+++ application/src/main/java/com/musicforall/config/security/CustomAuthenticationSuccessHandler.java
@@ -2,7 +2,6 @@ package com.musicforall.config.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
