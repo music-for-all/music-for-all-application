d5b272f0ce3fc390c595e949eebef6ce9c78a0c4
Ilia Nikolsky <nikolsky12@gmail.com>
2016-07-29 14:45:22 +0300

    Made test template

--- history/src/test/java/com/musicforall/history/service/HistoryServiceTest.java
+++ history/src/test/java/com/musicforall/history/service/HistoryServiceTest.java
@@ -6,6 +6,9 @@ import com.musicforall.history.handlers.events.TrackListenedEvent;
 import com.musicforall.history.model.History;
 import com.musicforall.history.util.ServicesTestConfig;
 import org.easymock.EasyMock;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Property;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
@@ -17,8 +20,11 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import java.util.List;
+
 import static org.easymock.EasyMock.anyObject;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * @author IliaNik on 28.07.2016.
@@ -32,48 +38,50 @@ import static org.junit.Assert.assertNotNull;
 @ActiveProfiles("dev")
 public class HistoryServiceTest {
 
-    static final Integer User_ID = 1111;
+    static final Integer USER_ID = 1111;
 
-    static final Integer Track_ID = 2222;
+    static final Integer TRACK_ID = 2222;
 
     @Autowired
     private Dao dao;
 
-    @Test
-    public void testEventListener() {
 
-        TrackListenedEvent trackListenedEvent = new TrackListenedEvent(Track_ID, User_ID);
+    private TrackListenedEvent trackListenedEvent;
+    private HistoryEventListener historyEventListener;
+    private HistoryService historyService;
 
+    @Before
+    public void setUp() {
+        trackListenedEvent = new TrackListenedEvent(TRACK_ID, USER_ID);
 
-        HistoryEventListener historyEventListener = PowerMock.createMock(HistoryEventListener.class);
+        historyEventListener = PowerMock.createPartialMock(HistoryEventListener.class,
+                "toHistoryEntity", new Class[]{TrackListenedEvent.class});
 
-        historyEventListener.handleTrackListened(trackListenedEvent);
-        EasyMock.expectLastCall().atLeastOnce();
-        EasyMock.replay(historyEventListener);
-
-        historyEventListener.handleTrackListened(trackListenedEvent);
-        EasyMock.verify(historyEventListener);
-
+        historyService = EasyMock.createMock(HistoryService.class);
     }
 
 
     @Test
-    public void testEventService() {
+    public void test() throws Exception {
+        PowerMock.expectPrivate(historyEventListener, "toHistoryEntity",
+                anyObject(History.class)).andReturn(new History());
 
-        TrackListenedEvent trackListenedEvent = new TrackListenedEvent(Track_ID, User_ID);
-
-        HistoryEventListener historyEventListener = new HistoryEventListener();
-
-        HistoryService historyService = EasyMock.createMock(HistoryService.class);
 
         historyService.record(anyObject(History.class));
-        EasyMock.expectLastCall().atLeastOnce();
-        EasyMock.replay(historyService);
+        EasyMock.expectLastCall();
+
+
+        EasyMock.replay(historyService, historyEventListener);
 
         historyEventListener.handleTrackListened(trackListenedEvent);
-        EasyMock.verify(historyService);
 
-        final History history = dao.get(History.class, savedHistory.getId());
-        assertNotNull(history);
+        EasyMock.verify(historyService, historyEventListener);
+
+
+        final DetachedCriteria detachedCriteria = DetachedCriteria.forClass(History.class)
+                .add(Property.forName("user_id").eq(USER_ID))
+                .add(Property.forName("track_id").eq(TRACK_ID));
+        final List<History> history = dao.getAllBy(detachedCriteria);
+        assertTrue(!history.isEmpty());
     }
 }
