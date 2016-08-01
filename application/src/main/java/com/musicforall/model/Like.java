da4fc5b51381da5ef6b047a5001854f916f392eb
Dimitri <excaliware@gmail.com>
2016-08-01 12:30:23 +0300

    Implement the Like model

---
+++ application/src/main/java/com/musicforall/model/Like.java
@@ -0,0 +1,106 @@
+package com.musicforall.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "likes")
+public class Like implements Serializable {
+
+    private static final long serialVersionUID = -6851477524231058789L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreationTimestamp
+    private Date date;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "track_id", nullable = true)
+    @Cascade(CascadeType.ALL)
+    private Track track;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @Cascade(CascadeType.ALL)
+    private User user;
+
+    public Like() {
+    }
+
+    public Like(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Track getTrack() {
+        return track;
+    }
+
+    public void setTrack(Track track) {
+        this.track = track;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    private void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, track, user);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Like other = (Like) obj;
+        return Objects.equals(this.id, other.id)
+                && Objects.equals(this.user, other.user)
+                && Objects.equals(this.track, other.track);
+    }
+
+    @Override
+    public String toString() {
+        return "Like{" +
+                "id=" + id +
+                ", date=" + date +
+                ", track_id=" + track.getId() +
+                ", user_id=" + user.getId() +
+                '}';
+    }
+}
