c48149cb47a131177c63821d6d401b5ef7b8ce01
andrey <gorbandrey@i.ua>
2016-08-11 18:54:03 +0300

    added config for social connection

---
+++ application/src/main/java/com/musicforall/config/SocialConfig.java
@@ -0,0 +1,113 @@
+package com.musicforall.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.social.UserIdSource;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+import org.springframework.social.config.annotation.EnableSocial;
+import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.*;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.connect.web.ReconnectFilter;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.google.api.Google;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+/**
+ * Created by andrey on 8/11/16.
+ */
+@Configuration
+@EnableSocial
+public class SocialConfig implements SocialConfigurer {
+
+    @Inject
+    private DataSource dataSource;
+
+    @Autowired
+    private ConnectionSignUp connectionSignUp;
+
+    @Override
+    public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer,
+                                       Environment environment) {
+        connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
+                environment.getProperty("spring.social.facebook.appId"),
+                environment.getProperty("spring.social.facebook.appSecret")));
+        connectionFactoryConfigurer.addConnectionFactory(new TwitterConnectionFactory(
+                environment.getProperty("spring.social.twitter.appId"),
+                environment.getProperty("spring.social.twitter.appSecret")));
+        GoogleConnectionFactory googleConnectionFactory = new GoogleConnectionFactory(
+                environment.getProperty("spring.social.google.appId"),
+                environment.getProperty("spring.social.google.appSecret"));
+        googleConnectionFactory.setScope("email");
+        connectionFactoryConfigurer.addConnectionFactory(googleConnectionFactory);
+    }
+
+    @Override
+    public UserIdSource getUserIdSource() {
+        return new UserIdSource() {
+            @Override
+            public String getUserId() {
+                System.out.println("S_________getUserId");
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication == null) {
+                    throw new IllegalStateException("Unable to get a ConnectionRepository: no user signed in");
+                }
+                return authentication.getName();
+            }
+        };
+    }
+
+    @Override
+    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator
+                                                                          connectionFactoryLocator) {
+        JdbcUsersConnectionRepository repository = new  JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+        repository.setConnectionSignUp(connectionSignUp);
+        return repository;
+    }
+
+    @Bean
+    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator,
+                                               ConnectionRepository connectionRepository) {
+        return new ConnectController(connectionFactoryLocator, connectionRepository);
+    }
+
+    @Bean
+    public ReconnectFilter apiExceptionHandler(UsersConnectionRepository usersConnectionRepository, UserIdSource userIdSource) {
+        return new ReconnectFilter(usersConnectionRepository, userIdSource);
+    }
+
+    @Bean
+    @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+    public Facebook facebook(ConnectionRepository repository) {
+        Connection<Facebook> connection = repository.findPrimaryConnection(Facebook.class);
+        return connection != null ? connection.getApi() : null;
+    }
+
+    @Bean
+    @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+    public Twitter twitter(ConnectionRepository repository) {
+        Connection<Twitter> connection = repository.findPrimaryConnection(Twitter.class);
+        return connection != null ? connection.getApi() : null;
+    }
+
+    @Bean
+    @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+    public Google google(ConnectionRepository repository) {
+        Connection<Google> connection = repository.findPrimaryConnection(Google.class);
+        return connection != null ? connection.getApi() : null;
+    }
+}
