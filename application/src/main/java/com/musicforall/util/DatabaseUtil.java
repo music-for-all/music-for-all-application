1ba05e458a0e442a0932386e7962237579c688ad
andrey <gorbandrey@i.ua>
2016-08-15 21:18:25 +0300

    fixed codacy issues

--- application/src/main/java/com/musicforall/util/DatabaseUtil.java
+++ application/src/main/java/com/musicforall/util/DatabaseUtil.java
@@ -1,11 +1,14 @@
 package com.musicforall.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,19 +17,24 @@ import java.sql.Statement;
  */
 public final class DatabaseUtil {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DatabaseUtil.class);
+
     private DatabaseUtil() {
     }
 
     public static void createUsersConnectionRepositoryTable(DataSource dataSource) {
         Statement statement;
-        try {
-            statement = dataSource.getConnection().createStatement();
+        try (Connection connection = dataSource.getConnection()) {
+            statement = connection.createStatement();
             statement.execute("select count(*) from UserConnection");
             statement.execute("DROP TABLE UserConnection");
-        } catch (SQLException e) { /*NOP*/ }
-        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        } catch (SQLException e) {
+            LOG.error("Table UserConnection does not exist", e);
+        }
+        final ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
         databasePopulator.addScript(new ClassPathResource("JdbcUsersConnectionRepository.sql",
                 JdbcUsersConnectionRepository.class));
         DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+        LOG.info("Table UserConnection successfully created");
     }
 }
