2dfeb0ece276347f7606e4ceacad6e871519e1f9
Pavel Podgorniy <podgpavel@gmail.com>
2016-08-15 17:31:51 +0300

    deleted unused import

--- application/src/main/java/com/musicforall/web/artist/ArtistRestController.java
+++ application/src/main/java/com/musicforall/web/artist/ArtistRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
