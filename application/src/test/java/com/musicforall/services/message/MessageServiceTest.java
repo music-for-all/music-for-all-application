7581755a1a7413d7cb26a112a53551279732e029
Ilia Nikolsky <nikolsky12@gmail.com>
2016-08-18 22:35:54 +0300

    Fixed test

--- application/src/test/java/com/musicforall/services/message/MessageServiceTest.java
+++ application/src/test/java/com/musicforall/services/message/MessageServiceTest.java
@@ -24,6 +24,8 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
@@ -67,60 +69,21 @@ public class MessageServiceTest {
         assertEquals(1, messages.length);
         assertEquals("Music For All", messages[0].getSubject());
         String body = GreenMailUtil.getBody(messages[0]).replaceAll("=\r?\n", "");
-        assertTrue(body.matches("(.*)" +
-                WelcomeMessage.getWelcomeText(SecurityUtil.currentUser().getUsername()) + "(.*)"));
 
+        String pattern = "(.*)(" +
+                WelcomeMessage.getWelcomeText(SecurityUtil.currentUser().getUsername()) + ")(.*)";
+
+        Matcher m = Pattern.compile(pattern).matcher(body);
+
+        assertTrue(m.find());
     }
 
 
     @After
     public void cleanup() {
         testSmtp.stop();
-    }
 
-
-}
-/*
-package com.musicforall.services.message;
-
-import com.musicforall.services.MessageService;
-import com.musicforall.util.ServicesTestConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-
-import javax.mail.MessagingException;
-
-
-/**
- * @author IliaNik on 14.08.2016.
- */
-
-/*
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {ServicesTestConfig.class})
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        MessageTestExecutionListener.class,
-        WithSecurityContextTestExecutionListener.class})
-@ActiveProfiles("dev")
-public class MessageServiceTest {
-
-    @Autowired
-    private MessageService messageService;
-
-    @Test
-    @WithUserDetails("user")
-    public void testSendMessageToRealEmail() throws MessagingException {
-        messageService.sendWelcomeMessage();
+        messageService.setPort(25);
+        messageService.setHost("smtp.gmail.com");
     }
 }
-*/
