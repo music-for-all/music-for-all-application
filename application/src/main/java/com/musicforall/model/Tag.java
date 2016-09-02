d74fceb11acefe2dac3db32db4cd3e7a6e06b006
andrey <gorbandrey@i.ua>
2016-09-02 20:16:52 +0300

    added NamedQuery for query of popular tags

--- application/src/main/java/com/musicforall/model/Tag.java
+++ application/src/main/java/com/musicforall/model/Tag.java
@@ -17,13 +17,22 @@ import java.util.Objects;
                 @NamedQuery(
                         name = Tag.ALL_LIKE_NAME_QUERY,
                         query = "from Tag where lower(name) like :name"
+                ),
+                @NamedQuery(
+                        name = Tag.POPULAR_TAGS_QUERY,
+                        query = "select tags.name from Track tt join tt.tags tags where tt.id in (:ids)" +
+                                " group by tags.name" +
+                                " order by count(tags.name) desc"
                 )
         }
 )
 @Table(name = "tags")
 public class Tag implements Serializable {
 
+    public static final String POPULAR_TAGS_QUERY = "most_popular_tags";
+
     public static final String ALL_LIKE_NAME_QUERY = "all_like_name";
+
     private static final long serialVersionUID = 5787383287840000175L;
     @Id
     @Size(min = 2, max = 30)
