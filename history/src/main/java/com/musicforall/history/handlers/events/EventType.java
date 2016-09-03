3837a308d2d388714ead01c70b69103d10cdd782
ENikolskiy <ENikolskiy@luxoft.com>
2016-09-03 14:19:22 +0300

    Method to decouple eventTypes.

--- history/src/main/java/com/musicforall/history/handlers/events/EventType.java
+++ history/src/main/java/com/musicforall/history/handlers/events/EventType.java
@@ -1,5 +1,9 @@
 package com.musicforall.history.handlers.events;
 
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * @author IliaNik on 17.07.2016.
  */
@@ -9,5 +13,14 @@ public enum EventType {
     TRACK_DELETED,
     PLAYLIST_ADDED,
     PLAYLIST_DELETED,
-    TRACK_LIKED
+    TRACK_LIKED;
+
+    public boolean in(final EventType... types) {
+        requireNonNull(types, "Types must not be null");
+
+        return Stream.of(types)
+                .filter(t -> this == t)
+                .findFirst()
+                .isPresent();
+    }
 }
