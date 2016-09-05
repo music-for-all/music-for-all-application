b8ca23d7b3458e07b6ca60da8a7fd050203407a0
andrey <gorbandrey@i.ua>
2016-09-05 17:02:51 +0300

    returned test for popular tags

--- application/src/test/java/com/musicforall/services/tag/TagServiceTest.java
+++ application/src/test/java/com/musicforall/services/tag/TagServiceTest.java
@@ -1,6 +1,12 @@
 package com.musicforall.services.tag;
 
+import com.musicforall.history.handlers.events.EventType;
+import com.musicforall.history.model.History;
+import com.musicforall.history.service.history.HistoryService;
 import com.musicforall.model.Tag;
+import com.musicforall.model.Track;
+import com.musicforall.services.recommendation.RecommendationService;
+import com.musicforall.services.track.TrackService;
 import com.musicforall.util.ServicesTestConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,8 +30,19 @@ import static org.junit.Assert.assertTrue;
 @ActiveProfiles("dev")
 public class TagServiceTest {
 
+    public static final String ALTERNATIVE = "alternative";
+
     @Autowired
     private TagService tagService;
+
+    @Autowired
+    private TrackService trackService;
+
+    @Autowired
+    private HistoryService historyService;
+
+    @Autowired
+    private RecommendationService recommendationService;
 
     @Test
     public void testSaveAllTagsGetAllTags() {
@@ -58,5 +75,23 @@ public class TagServiceTest {
         final Tag tag4 = new Tag("jazz");
         tagService.saveAll(Arrays.asList(tag1, tag2, tag3, tag4));
         assertEquals(3, tagService.getAllLike("rock").size());
+    }
+
+    @Test
+    public void testGetTheMostPopularTags() {
+        final Set<Tag> tags = new HashSet<>(Arrays.asList(new Tag("ROOK"), new Tag(ALTERNATIVE)));
+        final Track track = new Track("track1", "path2track1", tags);
+
+        trackService.save(track);
+        final Set<Tag> tags2 = new HashSet<>(Arrays.asList(new Tag("POP"), new Tag(ALTERNATIVE)));
+        final Track track2 = new Track("track2", "path2track2", tags2);
+
+        trackService.save(track2);
+        History history = new History(track.getId(), 1, new Date(), 1, EventType.TRACK_LISTENED);
+        historyService.record(history);
+        history = new History(track2.getId(), 1, new Date(), 1, EventType.TRACK_LISTENED);
+        historyService.record(history);
+        final Collection<Tag> tags_result = recommendationService.getPopularTags();
+        assertEquals(3, tags_result.size());
     }
 }
