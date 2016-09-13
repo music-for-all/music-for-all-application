6f62981f89bc832ad7d09dc9d16e95b91cd14140
Ilia Nikolsky <nikolsky12@gmail.com>
2016-09-14 00:00:35 +0300

    Fixed

--- history/src/main/java/com/musicforall/history/model/History.java
+++ history/src/main/java/com/musicforall/history/model/History.java
@@ -19,21 +19,14 @@ import java.util.Objects;
                 query = " from History history" +
                         " where history.userId IN :usersIds" +
                         " and day(current_date()) - day(history.date) <= 1" +
-                        " order by history.date desc"
-        )
-
-
-})
-@NamedQueries(
-        {
-                @NamedQuery(
+                        " order by history.date desc"),
+        @NamedQuery(
                         name = History.POPULAR_TRACKS_QUERY,
                         query = "select history.trackId" +
                                 " from History history" +
                                 " where history.eventType=:eventType" +
                                 " group by history.trackId" +
-                                " order by count(history.trackId) desc"
-                ),
+                                " order by count(history.trackId) desc"),
                 @NamedQuery(
                         name = History.TRACK_LIKES_COUNT_QUERY,
                         query = "select count(*) from History history " +
