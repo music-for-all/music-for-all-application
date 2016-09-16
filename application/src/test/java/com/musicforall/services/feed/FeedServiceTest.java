f2db270499c3c2c85efc72e449033627c350b30f
Ilia Nikolsky <nikolsky12@gmail.com>
2016-09-16 18:40:33 +0300

    Checkstyle

--- application/src/test/java/com/musicforall/services/feed/FeedServiceTest.java
+++ application/src/test/java/com/musicforall/services/feed/FeedServiceTest.java
@@ -81,42 +81,6 @@ public class FeedServiceTest {
 
     @Test
     public void testGetGroupedFollowingFeeds() {
-/* final User user = new User("Adolf", PASSWORD, "meinkampf@example.com"); 
-final User user1 = new User("Iosiv", PASSWORD, "tribunal@example.com"); 
-final User user2 = new User("Winston", PASSWORD, "UK@example.com"); 
-
-final Track track = new Track("Ray Charles – Mess around", "Mess around", "Ray Charles", null, LOC_1, null); 
-final Playlist playlist = new Playlist("Jazz", null, user2); 
-
-userService.save(user); 
-userService.save(user1); 
-userService.save(user2); 
-
-trackService.save(track); 
-playlistService.save(playlist); 
-
-final int USER1_ID = user1.getId(); 
-final int USER2_ID = user2.getId(); 
-
-followerService.follow(user.getId(), USER1_ID); 
-followerService.follow(user.getId(), USER2_ID); 
-
-final History history1 = new History(track.getId(), FeedServiceTest.PLAYLIST_ID, 
-new Date(new Date().getTime() + 1), USER1_ID, TRACK_LIKED); 
-
-final History history2 = new History(FeedServiceTest.TRACK_ID, playlist.getId(), 
-new Date(new Date().getTime() + 2), USER2_ID, PLAYLIST_ADDED);
-
-historyService.record(history1); 
-historyService.record(history2); 
-
-when(messageSource.getMessage(any(), any(), any())).thenReturn(testMessage); 
-
-final Feed feed1 = new Feed(testMessage + " " + 
-"Ray Charles – Mess around", history1.getDate()); 
-final Feed feed2 = new Feed(testMessage + " " + 
-"Jazz", history2.getDate());*/
-
         final History history1 = new History(TRACK_ID, PLAYLIST_ID,
                 new Date(new Date().getTime() + 1), USER1_ID, TRACK_LIKED);
 
@@ -126,7 +90,6 @@ final Feed feed2 = new Feed(testMessage + " " +
         when(followerService.getFollowingId(USER_ID)).thenReturn(Arrays.asList(2));
         when(userService.getUsersById(any())).thenReturn(Arrays.asList(user));
         when(historyService.getUsersHistories(any())).thenReturn(Arrays.asList(history1, history2));
-
         when(trackService.getAllByIds(any())).thenReturn(Arrays.asList(track));
         when(playlistService.getAllByIds(any())).thenReturn(Arrays.asList(playlist));
         when(track.getId()).thenReturn(TRACK_ID);
@@ -138,10 +101,8 @@ final Feed feed2 = new Feed(testMessage + " " +
 
         final Map<User, Collection<Feed>> followingHistories = feedService.getGroupedFollowingFeeds(USER_ID);
 
-        final Feed feed1 = new Feed(testMessage + " " +
-                "Ray Charles – Mess around", history1.getDate());
-        final Feed feed2 = new Feed(testMessage + " " +
-                "Jazz", history2.getDate());
+        final Feed feed1 = new Feed(testMessage + " " + "Ray Charles – Mess around", history1.getDate());
+        final Feed feed2 = new Feed(testMessage + " " + "Jazz", history2.getDate());
 
         assertTrue(followingHistories.get(user).stream().anyMatch(f -> f.equals(feed1)));
         assertTrue(followingHistories.get(user).stream().anyMatch(f -> f.equals(feed2)));
