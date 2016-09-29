601cc13d571f9db4f3550dba64587cf01530916a
Annalysses <pukhova.anastas@gmail.com>
2016-09-29 14:41:13 +0300

    add active row class for playing track row in tables

--- application/src/main/webapp/resources/js/player.js
+++ application/src/main/webapp/resources/js/player.js
@@ -12,69 +12,55 @@ jQuery(document).ready(function () {
         $("#artistInFoot").html(tr.artist);
     }
 
-    function playTrack(trackId) {
-        if (player.getTrackId() == trackId) {
-            player.resume();
-        }
-        else {
-            removePausingState();
+    function startTrack(trackId) {
             track.get(trackId).then(function (tr) {
                 player.play(tr);
                 writeToFoot(tr);
             });
-        }
     }
 
-    function removePausingState() {
-        if ($trackHtml) {
-            var prevBttn = $trackHtml.children().children().first();
-            prevBttn.removeClass('btn-warning').find('span').removeClass('glyphicon-pause');
-        }
+    function changeActiveTrackRow(currentRow) {
+        startTrack(currentRow[0].id);
+        $(".tracks-table tr").removeClass("active");
+        $(currentRow).closest("tr").addClass("active");
     }
-    
-    function changeStateButton() {
-        if ($trackHtml) {
-            var bttn = $trackHtml.children().children().first();
-            bttn.toggleClass('btn-warning').find('span').toggleClass('glyphicon-pause');
-        }
-    }
-    /* Make the play button to invoke play on the corresponding player. */
 
-    $(".tracks-table").on("click", ".play-pause-button", function () {
-        if (!$(this).hasClass('btn-warning')) {
-            playTrack($(this).closest("tr")[0].id);
-            $trackHtml = $(this).closest("tr");
+    $(".tracks-table").on("click", ".play-button", function () {
+        $trackHtml = $(this).closest("tr");
+        if (player.getTrackId() != $trackHtml[0].id) {
+            changeActiveTrackRow($trackHtml);
+        } else {
+            player.resume();
+            $trackHtml.addClass("active");
         }
-        else {
-            player.pause();
-        }
-        changeStateButton()
     });
 
-
+    $(".tracks-table").on("click", ".pause-button", function () {
+        player.pause();
+        $(this).removeClass("active");
+    });
+    
     $('#nextFooterBtn').bind('click', function() {
         if ($trackHtml.next()[0].id) {
-            playTrack($trackHtml.next()[0].id);
             $trackHtml = $trackHtml.next();
-            changeStateButton()
+            changeActiveTrackRow($trackHtml);
         }
     });
 
     $('#prevFooterBtn').bind('click', function() {
         if ($trackHtml.prev()[0].id) {
-            playTrack($trackHtml.prev()[0].id);
             $trackHtml = $trackHtml.prev();
-            changeStateButton()
+            changeActiveTrackRow($trackHtml);
         }
     });
 
     $('#playFooterBtn').bind('click', function() {
         player.resume();
-        changeStateButton()
+        $trackHtml.addClass("active");
     });
 
     $('#pauseFooterBtn').bind('click', function() {
         player.pause();
-        changeStateButton()
+        $trackHtml.removeClass("active");
     });
 });
