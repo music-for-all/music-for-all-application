44d550a939b2912f4f0d75f15752b02016f89d1a
andrey <gorbandrey@i.ua>
2016-10-02 12:28:24 +0300

    added controllers for upload and get picture

--- application/src/main/java/com/musicforall/web/file/FileController.java
+++ application/src/main/java/com/musicforall/web/file/FileController.java
@@ -5,10 +5,10 @@ import com.musicforall.files.manager.FileManager;
 import com.musicforall.model.Track;
 import com.musicforall.services.file.FileService;
 import com.musicforall.services.track.TrackService;
+import com.musicforall.util.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -29,9 +29,6 @@ import java.util.Optional;
 @Controller
 public class FileController {
     private static final Logger LOG = LoggerFactory.getLogger(FileController.class);
-
-    @Autowired
-    private ApplicationEventPublisher publisher;
 
     @Autowired
     private FileManager manager;
@@ -78,4 +75,35 @@ public class FileController {
     public String uploadFile() {
         return "uploadFile";
     }
+
+    @RequestMapping(value = "/files/picture", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> uploadPicFileHandler(
+            @RequestPart("file") MultipartFile file) {
+        LOG.info(("Streaming picture"));
+
+        final ResponseEntity<String> errorStatus = fileService.checkFile(file);
+        if (Objects.equals(errorStatus.getBody(), "ok")) {
+            return fileService.uploadPictureFile(SecurityUtil.currentUser(), file);
+        } else {
+            return errorStatus;
+        }
+    }
+
+    @RequestMapping(value = "/{pictureName:.+}", method = RequestMethod.GET)
+    public void getFileHandler(HttpServletResponse response, @PathVariable("pictureName") String name) {
+        LOG.info(String.format("Streaming file: %s\n", name));
+        final Optional<Path> filePath = manager.getPicturePathByName(name);
+
+        if (filePath.isPresent()) {
+            try {
+                Files.copy(filePath.get(), response.getOutputStream());
+            } catch (IOException e) {
+                LOG.error("Streaming failed! ", e);
+            }
+        } else {
+            LOG.error("File not found");
+        }
+    }
+
 }
