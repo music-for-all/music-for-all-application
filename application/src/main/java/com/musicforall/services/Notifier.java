2b4aa69f0ad8198385d2c671ae24917b4dc668dd
ENikolskiy <enikolskiy@luxoft.com>
2016-10-19 18:27:24 +0300

    create separate notifier for different notifications

--- application/src/main/java/com/musicforall/services/Notifier.java
+++ application/src/main/java/com/musicforall/services/Notifier.java
@@ -4,7 +4,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.Collections;
 import java.util.Queue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -18,7 +17,7 @@ public class Notifier {
     private final Queue<Notification> queue = new ConcurrentLinkedQueue<>();
 
     public <T> DeferredResult<T> deffer(Callable<T> callable) {
-        final DeferredResult<T> result = new DeferredResult<>(null, Collections.emptyList());
+        final DeferredResult<T> result = new DeferredResult<>();
         final Notification<T> notification = new Notification<T>(result, callable);
 
         final Runnable remove = () -> queue.removeIf(n -> n.getId().equals(notification.getId()));
