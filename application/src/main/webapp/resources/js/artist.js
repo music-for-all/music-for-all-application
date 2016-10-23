fb65ac93c1371954359bcfbd18b2f651bddf2efb
Dimitri <excaliware@gmail.com>
2016-10-23 17:33:04 +0300

    Add the methods getTopTracks and getTopAlbums

--- application/src/main/webapp/resources/js/artist.js
+++ application/src/main/webapp/resources/js/artist.js
@@ -1,10 +1,10 @@
 "use strict";
 
-function Artist() {
+function Artist(name) {
 
     var self = this;
 
-    self.getInfo = function (name) {
+    self.getInfo = function() {
 
         var url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + name +
             "&api_key=2b35547bd5675d8ecb2b911ee9901f59&format=json";
@@ -17,7 +17,26 @@ function Artist() {
             }).fail(function (xhr, status, errorThrown) {
                 console.log("Error: " + errorThrown);
                 console.log("Status: " + status);
-                console.dir(xhr);
             }));
+    };
+
+    self.getTopTracks = function() {
+
+        var url = "/tracks/topTracksOf/" + name;
+
+        return $.when($.ajax({
+            type: "GET",
+            url: url
+        }));
+    }
+
+    self.getTopAlbums = function() {
+
+        var url = "/tracks/topAlbumsOf/" + name;
+
+        return $.when($.ajax({
+            type: "GET",
+            url: url
+        }));
     }
 }
--- application/src/main/webapp/resources/js/artistProfile.js
+++ application/src/main/webapp/resources/js/artistProfile.js
@@ -2,17 +2,41 @@
 
 $(document).ready(function() {
 
-    var artist = new Artist();
-    var name = $(".panel-title").text();
+    var artistName = $(".panel-title").text();
+    var artist = new Artist(artistName);
 
-    artist.getInfo(name)
+    _.templateSettings.variable = "data";
+    var trackTable = _.template(
+        $("script.addTrackRowTemplate").html()
+    );
+
+    artist.getInfo()
         .then(function (data) {
             var artist = data.artist;
-            console.log(artist);
+
             $("#artist-name").text(artist.name);
 
             $("#artist-picture").attr("src", artist.image[3]["#text"]);
 
             $("#artist-bio").html(artist.bio.summary);
+        });
+
+    artist.getTopTracks()
+        .then(function (response) {
+
+            response.forEach(function (track) {
+                $("#tracks").find("thead").after(
+                    trackTable(track)
+                );
+            });
+        });
+
+    artist.getTopAlbums()
+        .then(function (response) {
+
+            response.forEach(function (album) {
+                $("#albums").find("thead").after(
+                    $("<tr>").text(album));
+            });
         });
 });
