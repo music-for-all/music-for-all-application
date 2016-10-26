90f3cf3f3127b792ddf35d26f74ed6da4c06539d
ENikolskiy <enikolskiy@luxoft.com>
2016-10-26 17:56:19 +0300

    Fix test

--- application/src/test/java/com/musicforall/util/ServicesTestConfig.java
+++ application/src/test/java/com/musicforall/util/ServicesTestConfig.java
@@ -2,15 +2,23 @@ package com.musicforall.util;
 
 import com.musicforall.cache.NotificationCacheProvider;
 import com.musicforall.cache.StreamCacheProvider;
+import com.musicforall.common.cache.CacheProvider;
 import com.musicforall.config.CacheConfig;
 import com.musicforall.config.HibernateConfigDev;
 import com.musicforall.config.WebSocketConfig;
 import com.musicforall.config.security.SecurityConfig;
 import com.musicforall.history.HistorySpringConfig;
+import com.musicforall.model.Track;
+import com.musicforall.notifications.Notification;
 import com.musicforall.services.DbPopulateService;
 import com.musicforall.services.file.FileServiceImpl;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.*;
+
+import java.util.Collection;
+
+import static com.musicforall.config.CacheConfig.NOTIFICATION;
+import static com.musicforall.config.CacheConfig.STREAM;
 
 /**
  * Created by Pukho on 19.06.2016.
@@ -20,6 +28,7 @@ import org.springframework.context.annotation.*;
 @ComponentScan(
         value = {"com.musicforall.services",
                 "com.musicforall.common",
+                "com.musicforall.notifications",
                 "com.musicforall.web.stream"},
         excludeFilters =
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value =
@@ -29,13 +38,13 @@ import org.springframework.context.annotation.*;
         HistorySpringConfig.class, CacheConfig.class, WebSocketConfig.class})
 public class ServicesTestConfig {
 
-    @Bean
-    public NotificationCacheProvider cacheNews() {
+    @Bean(name = NOTIFICATION)
+    public CacheProvider<Integer, Collection<Notification>> cacheNews() {
         return new NotificationCacheProvider();
     }
 
-    @Bean
-    public StreamCacheProvider cacheStream() {
+    @Bean(name = STREAM)
+    public CacheProvider<Integer, Track> cacheStream() {
         return new StreamCacheProvider();
     }
 }
